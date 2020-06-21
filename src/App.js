c339c26c799d80fa4a2cabc0f7fbf50dbef47273
Salahuddin Muhammad <45813964+salahsaifjalal@users.noreply.github.com>
2020-06-21 14:57:28 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Salahuddin Muhammad is working.
+          Salahuddin Muhammad is making Expenses Tracker App.
         </p>
         <a
           className="App-link"
